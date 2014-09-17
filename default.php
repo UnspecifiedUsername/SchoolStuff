400ce71cb70c15a184059a820975bffa1b0ad101
UnspecifiedUsername <nick.f.emerson@gmail.com>
2014-09-16 20:09:50 -0400

    Create default.php

---
+++ default.php
@@ -0,0 +1,58 @@
+<html>
+<title>Peg Problem</title>
+<body background="h.jpg">
+<font face="Trebuchet MS, Arial, Helvetica, sans-serif">
+<center> <h><font size="+4">Welcome to UnspecifiedPerson's Pegboard Area Calculator!</font></h></center>
+<center> <p> Exterior Pegs&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Interior Pegs
+<script>
+function calc(form) {
+
+var C = "0";
+var A = document.getElementById("num1").value;
+var B = document.getElementById("num2").value;
+
+C = parseInt(B)+((parseInt(A)/2)-1); 
+
+document.getElementById("result").innerHTML = C;
+return false;
+}
+</script>
+<body>
+
+<center><input type="text" id="num1" name="num1" />
+<input type="text" id="num2" name="num2" />
+<br />
+<input type="button" value="Find Peg Area" onClick="calc(this)"></center>
+
+<script>
+
+function NewPage() {
+  document.location.href= "http://www.nick.net16.net/PegProblem.html"
+}
+
+</script>
+
+<body>
+
+<input type="button" value="View Full Writeup!" onClick="NewPage()">
+<script>
+
+function NewPage() {
+  document.location.href= "https://github.com/UnspecifiedUsername/SchoolStuff"
+}
+
+</script>
+
+<body>
+
+<input type="button" value="View Source on GitHub!" onClick="NewPage()">
+
+
+</body>
+<p id="result" name="r1">
+<br />
+</p>
+</font>
+</body>
+</body>
+</html>
